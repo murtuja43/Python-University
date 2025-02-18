3838c952fb58f9ad5f3267a6b60680c2c36f8060
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-02-18 22:47:58 +0500

    added notes in print option to understand what to expect

--- problem 1-10.py
+++ problem 1-10.py
@@ -1,22 +1,24 @@
 # problem 1
 a = int(input("Whats a for Perameter P:"))
 p = 4 * a
-print(p)
+print("Prarmeter:", p)
 
 
 
 # problem 2
 a = int(input("Whats a for area S:"))
 S = a * a
-print(S)
+print("Area:", S)
 
 
 
 # problem 3
 a = int(input("Rectangular side a: "))
 b = int(input("Rectangular side b: "))
+S = a * b
 P = 2 * (a + b)
-print(P)
+print("Area:", S)
+print("Perameter:", P)
 
 
 
@@ -24,7 +26,7 @@ print(P)
 d = int(input("Whats d: "))
 p = 3.14
 L = p * d
-print(L)
+print("Circumference:", L)
 
 
 
@@ -32,8 +34,8 @@ print(L)
 a = int(input("Whats a of a edge of a cube:"))
 V = a ** 3
 S = 6 * (a ** 2)
-print(V)
-print(S)
+print("Volume:", V)
+print("Area:", S)
 
 
 
@@ -43,7 +45,8 @@ b = int(input("Whats edge b for rectangular: "))
 c = int(input("Whats edge c for rectangular: "))
 V = a * b * c
 S = 2 * (a * b + b * c + a * c)
-print(V)
+print("Volume:", V)
+print("Surface Area:", S)
 
 
 
@@ -52,8 +55,8 @@ r = int(input("Whats r for circle: "))
 p = 3.14
 L = 2 * p * r
 S = p * (r ** 2)
-print(L)
-print(S)
+print("Circumference:", L)
+print("Area:", S)
 
 
 
@@ -61,7 +64,7 @@ print(S)
 a = int(input("Whats a for arithmetic mean: "))
 b = int(input("Whats b for arithmetic mean: "))
 A = (a + b) / 2
-print(A)
+print("Arithmetic Mean:", A)
 
 
 
@@ -70,7 +73,7 @@ a = int(input("What's the first non-negative number a: "))
 b = int(input("What's the first non-negative number b: "))
 
 geometric_mean = (a * b) ** .5
-print(geometric_mean)
+print("Geometric mean:", geometric_mean)
 
 
 # problem 10
@@ -82,7 +85,7 @@ difference_of_squares = a ** 2 - b ** 2
 product_of_squares = a ** 2 * b ** 2
 quotient_of_squares = a ** 2 / b ** 2
 
-print(sum_of_squares)
-print(difference_of_squares)
-print(product_of_squares)
-print(quotient_of_squares)
+print("Sum of their squres:",sum_of_squares)
+print("Difference of their squres:", difference_of_squares)
+print("Product of their squres:", product_of_squares)
+print("Quotient of their squres:", quotient_of_squares)
