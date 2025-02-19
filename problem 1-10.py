55c02ccdf36ac526e7c1170b782879ca3d105ea3
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-02-19 22:55:01 +0500

    added 3 quote comments

--- problem 1-10.py
+++ problem 1-10.py
@@ -3,6 +3,7 @@ a = int(input("Whats a for Perameter P:"))
 p = 4 * a
 print("Prarmeter:", p)
 
+'''problem 1 solved'''
 
 
 # problem 2
@@ -10,6 +11,7 @@ a = int(input("Whats a for area S:"))
 S = a * a
 print("Area:", S)
 
+'''problem 2 solved'''
 
 
 # problem 3
@@ -20,6 +22,7 @@ P = 2 * (a + b)
 print("Area:", S)
 print("Perameter:", P)
 
+'''problem 3 solved'''
 
 
 # problem 4
@@ -28,6 +31,7 @@ p = 3.14
 L = p * d
 print("Circumference:", L)
 
+'''problem 4 solved'''
 
 
 # problem 5
@@ -37,6 +41,7 @@ S = 6 * (a ** 2)
 print("Volume:", V)
 print("Area:", S)
 
+'''problem 5 solved'''
 
 
 # problem 6
@@ -48,6 +53,7 @@ S = 2 * (a * b + b * c + a * c)
 print("Volume:", V)
 print("Surface Area:", S)
 
+'''problem 6 solved'''
 
 
 # problem 7
@@ -58,6 +64,7 @@ S = p * (r ** 2)
 print("Circumference:", L)
 print("Area:", S)
 
+'''problem 7 solved'''
 
 
 # problem 8
@@ -66,6 +73,7 @@ b = int(input("Whats b for arithmetic mean: "))
 A = (a + b) / 2
 print("Arithmetic Mean:", A)
 
+'''problem 8 solved'''
 
 
 # problem 9
@@ -74,6 +82,8 @@ b = int(input("What's the first non-negative number b: "))
 
 geometric_mean = (a * b) ** .5
 print("Geometric mean:", geometric_mean)
+
+'''problem 9 solved'''
 
 
 # problem 10
@@ -89,3 +99,5 @@ print("Sum of their squres:",sum_of_squares)
 print("Difference of their squres:", difference_of_squares)
 print("Product of their squres:", product_of_squares)
 print("Quotient of their squres:", quotient_of_squares)
+
+'''problem 10 solved'''
